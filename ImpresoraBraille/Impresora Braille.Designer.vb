a4041df05b68050f978da3fc630737d767859e7e
Enrique Walter Philippeaux <quique18c@gmail.com>
2019-09-04 15:31:15 -0300

    conecta al com, muestra bitmap

--- ImpresoraBraille/Impresora Braille.Designer.vb
+++ ImpresoraBraille/Impresora Braille.Designer.vb
@@ -23,48 +23,148 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
         Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.LabelEstado = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.ButtonReloadCom = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'TextBox1
+        'RichTextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(392, 12)
-        Me.TextBox1.Multiline = True
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(380, 381)
-        Me.TextBox1.TabIndex = 0
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(469, 437)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(6, 69)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(50, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Estado:"
+        '
+        'LabelEstado
+        '
+        Me.LabelEstado.AutoSize = True
+        Me.LabelEstado.Location = New System.Drawing.Point(71, 69)
+        Me.LabelEstado.Name = "LabelEstado"
+        Me.LabelEstado.Size = New System.Drawing.Size(96, 13)
+        Me.LabelEstado.TabIndex = 2
+        Me.LabelEstado.Text = "DESCONECTADO"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ButtonReloadCom)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.LabelEstado)
+        Me.GroupBox1.Controls.Add(Me.ComboBox1)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(599, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(173, 93)
+        Me.GroupBox1.TabIndex = 7
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Puerto Serie"
+        '
+        'ButtonReloadCom
+        '
+        Me.ButtonReloadCom.BackgroundImage = Global.ImpresoraBraille.My.Resources.Resources.iconfinder_sync_126579
+        Me.ButtonReloadCom.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ButtonReloadCom.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonReloadCom.Location = New System.Drawing.Point(146, 16)
+        Me.ButtonReloadCom.Name = "ButtonReloadCom"
+        Me.ButtonReloadCom.Size = New System.Drawing.Size(21, 21)
+        Me.ButtonReloadCom.TabIndex = 5
+        Me.ButtonReloadCom.UseVisualStyleBackColor = True
         '
         'Button1
         '
-        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.Location = New System.Drawing.Point(12, 399)
+        Me.Button1.Location = New System.Drawing.Point(9, 43)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(760, 50)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Imprimir"
+        Me.Button1.Size = New System.Drawing.Size(158, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Conectar"
         Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(6, 19)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Puerto:"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(60, 16)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(80, 21)
+        Me.ComboBox1.TabIndex = 2
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(599, 111)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "Mostrar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(680, 111)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "Enviar"
+        Me.Button3.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(784, 461)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.MaximizeBox = False
         Me.MaximumSize = New System.Drawing.Size(800, 500)
         Me.MinimumSize = New System.Drawing.Size(800, 500)
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Impresora Braille"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents Button1 As Button
     Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents LabelEstado As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ButtonReloadCom As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
 End Class
--- ImpresoraBraille/Impresora Braille.vb
+++ ImpresoraBraille/Impresora Braille.vb
@@ -1,12 +1,85 @@
 ﻿Public Class Form1
 #Region "variables"
-    Dim a(,) As Boolean = {{True, False}, {False, False}, {False, False}}
+    Dim arrayHoja(6, 71) As Byte
 #End Region
+
+    Sub GetSerialPortNames()
+        ' Show all available COM ports.
+        ComboBox1.Items.Clear()
+
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            ComboBox1.Items.Add(sp)
+        Next
+
+        ComboBox1.SelectedIndex = 0
+    End Sub
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        GetSerialPortNames()
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
 
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub ButtonReloadCom_Click(sender As Object, e As EventArgs) Handles ButtonReloadCom.Click
+        GetSerialPortNames()
+    End Sub
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            If Not SerialPort1.IsOpen Then
+                SerialPort1.BaudRate = 115200
+                SerialPort1.PortName = ComboBox1.SelectedItem.ToString()
+                SerialPort1.Open()
+                LabelEstado.Text = "CONECTADO"
+                Button1.Text = "DESCONECTAR"
+            Else
+                SerialPort1.Close()
+                LabelEstado.Text = "DESCONECTADO"
+                Button1.Text = "CONECTAR"
+            End If
+        Catch ex As Exception
+            MsgBox(ex)
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        For i_ejeY As Integer = arrayHoja.GetLowerBound(1) To arrayHoja.GetUpperBound(1)
+            For i_ejeX As Integer = arrayHoja.GetLowerBound(0) To arrayHoja.GetUpperBound(0)
+                arrayHoja(i_ejeX, i_ejeY) = 0
+            Next
+            arrayHoja(0, i_ejeY) = 128
+        Next
+
+        RichTextBox1.Clear()
+
+        Dim contador_renglones As Integer = 0
+        For i_ejeY As Integer = arrayHoja.GetLowerBound(1) To arrayHoja.GetUpperBound(1)
+            For i_ejeX As Integer = arrayHoja.GetLowerBound(0) To arrayHoja.GetUpperBound(0)
+
+                RichTextBox1.Text += Convert.ToString(arrayHoja(i_ejeX, i_ejeY), 2).PadLeft(8, "0")
+                If i_ejeX < arrayHoja.GetUpperBound(0) Then
+                    RichTextBox1.Text += "-"
+                End If
+            Next
+            RichTextBox1.Text += Environment.NewLine
+
+            contador_renglones += 1
+
+            If contador_renglones = 3 Then
+                RichTextBox1.Text += Environment.NewLine
+                contador_renglones = 0
+            End If
+        Next
+    End Sub
+    Sub SendArrayHoja()
+        For i_ejeY As Integer = arrayHoja.GetLowerBound(1) To arrayHoja.GetUpperBound(1)
+            For i_ejeX As Integer = arrayHoja.GetLowerBound(0) To arrayHoja.GetUpperBound(0)
+
+            Next
+
+        Next
     End Sub
 End Class
